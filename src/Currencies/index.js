dcdc310712c5e6f18813ece94c09d0d45f704cdd
Olga Cybulska <olgacybulska7@wp.pl>
2023-10-31 19:07:20 +0100

    table added

---
+++ src/Currencies/index.js
@@ -0,0 +1,23 @@
+const Currencies = [
+    {
+       short: "EUR",
+       name: "Euro",
+       rate: 4.4689,
+    },
+    {
+       short: "USD",
+       name: "Dolar amerykański",
+       rate: 4.2251,
+    },
+    {
+       short: "GBT",
+       name: "Funt brytyjski",
+       rate: 5.1325
+    },
+    {
+        short: "CHF",
+        name: "Frank Szwajcarski",
+        rate: 4.6455
+     },
+ ];
+  export default Currencies;

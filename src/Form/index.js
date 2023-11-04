6e140f40e61874d210fa17ea99d9e80142937204
Olga Cybulska <olgacybulska7@wp.pl>
2023-11-04 17:28:23 +0100

    Result function added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 import { useState } from "react";
 import { currencies } from "../currecies";
+import Result from "./Result";
 
-const Form = ( {calculateResult, result }) => {
-  
+const Form = ({ calculateResult, result }) => {
   const [amount, setAmount] = useState("");
   const [currency, setCurrecy] = useState(currencies[0].short);
 
@@ -13,51 +13,47 @@ const Form = ( {calculateResult, result }) => {
   };
 
   return (
-    <form className="form" onSubmit={onFormSubmit}>
-      <fieldset className="form__fildset">
-        <legend className="form__legend">Kalkulator walut </legend>
-        <p>Wpisz kwotę w złotych, którą chcesz wymienić:</p>
-        <div>
-          <label>
-            <input
-              className="form__field"
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              required
-              type="number"
-              min="1"
-              step="0.01"
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            <select
-              className="form__field"
-              value={currency}
-              onChange={({ target }) => setCurrecy(target.value)}
-            >
-              {currencies.map((currency) => (
-                <option 
-                key={currency.short} 
-                value={currency.short}
-                >
-                  {currency.name}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <p>
-          <strong>Przelicz:</strong>
-        </p>
-        <button className="form__button">
-          [PLN] na [EUR]:
-        </button>
-      </fieldset>
-      <p className="form__text--important">*pole wymagane</p>
-     <div> {amount} {currency} {}</div>
-    </form>
+    <>
+      <form className="form" onSubmit={onFormSubmit}>
+        <fieldset className="form__fildset">
+          <legend className="form__legend">Kalkulator walut </legend>
+          <p>Wpisz kwotę w złotych, którą chcesz wymienić:</p>
+          <div>
+            <label>
+              <input
+                className="form__field"
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                required
+                type="number"
+                min="1"
+                step="0.01"
+              />
+            </label>
+          </div>
+          <div>
+            <p>Wybierz walutę, na którą chcesz wymienić:</p>
+            <label>
+              <select
+                className="form__field"
+                value={currency}
+                onChange={({ target }) => setCurrecy(target.value)}
+              >
+                {currencies.map((currency) => (
+                  <option key={currency.short} value={currency.short}>
+                    {currency.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <button className="form__button">Przelicz:</button>
+        </fieldset>
+        <p className="form__text--important">*pole wymagane</p>
+      </form>
+
+      <Result result={result} />
+    </>
   );
 };
 
